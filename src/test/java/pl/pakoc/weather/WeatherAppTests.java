1acd8be09ca367487374d9bf9e83f98fa86e7f2a
Paweł <p.k@git.com>
2019-08-19 23:11:33 +0200

    Revert "add initial test as ignore"
    This reverts commit cb198bad4995e5a386249804a6cc0748f2d898aa.

--- src/test/java/pl/pakoc/weather/WeatherAppTests.java
+++ src/test/java/pl/pakoc/weather/WeatherAppTests.java
@@ -4,14 +4,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.junit.Ignore;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class WeatherAppTests {
 
 	@Test
-	@Ignore
 	public void contextLoads() {
 	}
 
